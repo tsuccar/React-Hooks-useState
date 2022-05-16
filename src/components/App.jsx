db2b777dc35e66467fba3f616631ec21466546f6
tsuccar <tsuccar@users.noreply.github.com>
2022-05-15 20:01:48 -0400

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  const [count, setCount] = useState(0);
+
+  console.log(count);
+
+  return (
+    <div className="container">
+      <h1> {count} </h1>
+      <button onClick={decrease}> - </button>
+      <button onClick={increase}> + </button>
+    </div>
+  );
 }
 
 export default App;
